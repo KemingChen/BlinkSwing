bc42a331283da8b2eb464e5701f640c5bff70781
Keming <believe75467@gmail.com>
2013-09-23 16:00:47 +0800

    加入 LINEAR_ACCELERATION Sensor範例 在Console 視窗顯示 y軸值

--- src/com/example/mycanvas/MainActivity.java
+++ src/com/example/mycanvas/MainActivity.java
@@ -1,5 +1,11 @@
 package com.example.mycanvas;
 
+import java.util.List;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -9,9 +15,14 @@ import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ToggleButton;
 
-public class MainActivity extends Activity
+public class MainActivity extends Activity implements SensorEventListener
 {
+	float gX = 0;
+	float gY = 0;
+	float gZ = 0;
 	MyCanvas myCanvas;
+	private SensorManager sensorManager;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -19,6 +30,7 @@ public class MainActivity extends Activity
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 		setContentView(R.layout.activity_main);
 		myCanvas = (MyCanvas) findViewById(R.id.myCanvas1);
+		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 		final Button startButton = ((Button) findViewById(R.id.button_start));
 
 		((Button) findViewById(R.id.button1)).setOnClickListener(new Button.OnClickListener()
@@ -34,6 +46,7 @@ public class MainActivity extends Activity
 		startButton.setOnClickListener(new Button.OnClickListener()
 		{
 			int counter = 0;
+
 			@Override
 			public void onClick(View v)
 			{
@@ -51,12 +64,14 @@ public class MainActivity extends Activity
 				counter++;
 			}
 		});
-		((ToggleButton) findViewById(R.id.toggleButton1)).setOnCheckedChangeListener(
-				new CompoundButton.OnCheckedChangeListener() {
-		    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-		        myCanvas.normalShow = !myCanvas.normalShow; 
-		    }
-		});;
+		((ToggleButton) findViewById(R.id.toggleButton1)).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+		{
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+			{
+				myCanvas.normalShow = !myCanvas.normalShow;
+			}
+		});
+		;
 	}
 
 	@Override
@@ -81,6 +96,7 @@ public class MainActivity extends Activity
 		super.onPause();
 		myCanvas.stopBlink();
 		System.out.println("onPause");
+		sensorManager.unregisterListener(this);
 	}
 
 	@Override
@@ -88,5 +104,38 @@ public class MainActivity extends Activity
 	{
 		super.onResume();
 		System.out.println("onResume");
+		SetSensor();
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+	}
+
+	@SuppressWarnings("deprecation")
+	protected void SetSensor()
+	{
+		List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_LINEAR_ACCELERATION);
+		// 如果有取到該手機的方位感測器，就註冊他。
+		if (sensors.size() > 0)
+		{
+			// registerListener必須要implements SensorEventListener，
+			// 而SensorEventListener必須實作onAccuracyChanged與onSensorChanged
+			// 感應器註冊
+			sensorManager.registerListener(this, (Sensor) sensors.get(0), SensorManager.SENSOR_DELAY_NORMAL);
+		}
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy)
+	{
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event)
+	{
+		// TODO Auto-generated method stub
+		float[] values = event.values;
+		gX = values[0];
+		gY = values[1];
+		gZ = values[2];
+		System.out.println(gY);
 	}
 }
